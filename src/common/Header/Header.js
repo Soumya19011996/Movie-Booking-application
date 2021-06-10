deb3bc016341f88b66c7379bf19680ca5785e559
Peri <perisoumya@gmai.com>
2021-06-11 00:14:02 +0530

    checkpoint 2

---
+++ src/common/Header/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./Header.css"
+import "../../assets.logo.svg";
+import logo from '../../assets/logo.svg';
+
+
+
+class Header extends React.Component{
+render(){
+
+    return <div>
+        <div className="header">
+            <div className="logo"><img src={logo} alt='logo'/></div>
+        </div>;
+    </div>
+    
+}
+}
+export default Header;
